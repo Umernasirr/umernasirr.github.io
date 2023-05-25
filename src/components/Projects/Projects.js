cb5114a4a9a0ee37fb499c811893deef45146a23
Umer Nasir <umernasirr@gmail.com>
2023-05-25 23:19:58 +0500

    project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,7 +23,8 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          <Col md={1} />
+          <Col md={5} className="project-card">
             <ProjectCard
               imgPath={liveRecruit}
               imgPath2={liveRecruit1}
@@ -34,41 +35,43 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={5} className="project-card">
             <ProjectCard
               imgPath={neighborschools}
               imgPath2={neighborschools1}
               isBlog={false}
               title="NeighborSchools"
-              description="Neighborschools is a web application built on Next.js and powered by GraphQL, designed to connect parents with reliable daycare centers for their children and assist daycare centers in obtaining the necessary licenses to operate in the United States. At the same time, daycare centers can utilize the platform to streamline the licensing process by accessing essential resources, and receiving guidance from experienced professionals."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              description="Neighborschools is a web application built on Next.js and powered by GraphQL, designed to connect parents with reliable daycare centers for their children and assist daycare centers in obtaining the necessary licenses to operate in the United States."
               demoLink="https://www.neighborschools.com/"
             />
           </Col>
+          <Col md={1} />
 
-          <Col md={4} className="project-card">
+          <Col md={1} />
+
+          <Col md={5} className="project-card">
             <ProjectCard
               imgPath={dobbies}
               imgPath2={dobbies1}
               isBlog={false}
               title="Dobbies"
               description="Dobbies goes beyond the ordinary garden center experience, aiming to enhance lives and foster connections between people, their communities, and the environment. With a captivating web portfolio built on React for the frontend and Node for the backend, Dobbies offers meticulously curated product ranges both in-store and online, along with top-notch services, delightful restaurants and cafes, and immersive experiences."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
               demoLink="https://www.dobbies.com/"
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={5} className="project-card">
             <ProjectCard
               imgPath={sooper}
               imgPath2={sooper1}
               isBlog={false}
               title="Sooper App"
-              description="Sooper is an innovative ecommerce application designed to revolutionize online shopping, built with React for the frontend and powered by Node.js and Express for the backend. With a sleek and intuitive user interface, Sooper offers a seamless browsing and purchasing experience for customers. From fashion and electronics to home decor and more, the platform showcases a diverse range of products from trusted sellers. Sooper also incorporates secure payment gateways and efficient order processing, ensuring a smooth transaction process."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              description="Sooper is an innovative ecommerce application designed to revolutionize online shopping, built with React for the frontend and powered by Node.js and Express for the backend. With a sleek and intuitive user interface, Sooper offers a seamless browsing and purchasing experience for customers. From fashion and electronics to home decor and more, the platform showcases a diverse range of products from trusted sellers."
               demoLink="https://www.sooper.com.br/"
             />
           </Col>
+
+          <Col md={1} />
         </Row>
       </Container>
     </Container>
