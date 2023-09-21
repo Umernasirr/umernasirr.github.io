e46eacc721f332be38ff07f26d2663e4a01f0f85
Umer Nasir <umernasirr@gmail.com>
2023-09-21 14:08:15 +0500

    cleaning code

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-  HashRouter,
-} from "react-router-dom";
+import { Route, Routes, Navigate, HashRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
