d2cc082dbc40450fa1f38d64cebd034d0e2303fb
Umer Nasir <umernasirr@gmail.com>
2024-01-16 22:23:47 +0500

    changing cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./Umer Nasir.pdf";
+import pdf from "./Umer_Nasir.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
